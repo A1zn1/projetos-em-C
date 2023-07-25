06fe905fd10318ed034b737f5966d01b3b6466be
gustavosarubbi <gustavo.sarubbi1@gmail.com>
2023-07-25 02:58:43 -0300

    versao final

--- jogo da forca.c
+++ jogo da forca.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <string.h>
 
+#define MAX_TENTATIVAS 5
 #define TAMANHO_PALAVRAS 20
 #define NUM_PALAVRAS 8
 
@@ -11,17 +12,29 @@ int achou = 0;
 int tentativas = 0;
 char chutes[24];
 char palavrasecreta[20];
-
+int letras_acertada[26];
 char palavras[NUM_PALAVRAS][TAMANHO_PALAVRAS]={
-    "LARANJA"
-    "GIRAFA"
-    "AVIAO"
-    "COMPUTADOR"
-    "MONTANHA"
-    "LIVRO"
-    "CANETA"
+    "LARANJA",
+    "GIRAFA",
+    "AVIAO",
+    "COMPUTADOR",
+    "MONTANHA",
+    "LIVRO",
+    "CANETA",
     "BISCOITO"    
 };
+
+void verificaacertos(){
+    
+    int letras_acertadas = 0;
+        for (int i = 0; i < strlen(palavrasecreta); i++) {
+            if (letras_acertada[palavrasecreta[i] - 'A'] == 1) {
+                letras_acertadas++;
+            }
+        }
+    
+}
+
 void escolherpalavra(){
     srand(time(NULL));
     int randomico = rand()%NUM_PALAVRAS;
@@ -30,20 +43,32 @@ void escolherpalavra(){
 
 void verificaletra(){   
     for (int i = 0; i < strlen(palavrasecreta); i++){
-        achou = 0;
-        for (int j = 0; j < tentativas; j++){
-            if (chutes[j] == palavrasecreta[i]){
-                achou = 1;
-                break;
-            } 
-        }
-        if (achou){
-            printf("%c", palavrasecreta[i]);
+        if (letras_acertada[palavrasecreta[i] - 'A'] == 1){
+            printf("%c ", palavrasecreta[i]);
         }
         else{
             printf("_ ");
-        }    
+        }
     }
+    printf("\n");
+}
+
+void inicializar_letra_acertada(){
+    for(int i = 0; i < 26; i++){
+        letras_acertada[i] = 0;
+    }
+}
+
+void desenhar_forca(){
+    int erros = tentativas;
+    printf("  _______       \n");
+    printf(" |/      |      \n");
+    printf(" |      %c%c%c  \n", (erros >= 1 ? '(' : ' '), (erros >= 1 ? '_' : ' '), (erros >= 1 ? ')' : ' '));
+    printf(" |      %c%c%c  \n", (erros >= 3 ? '\\' : ' '), (erros >= 2 ? '|' : ' '), (erros >= 4 ? '/' : ' '));
+    printf(" |       %c     \n", (erros >= 2 ? '|' : ' '));
+    printf(" |      %c %c   \n", (erros >= 5 ? '/' : ' '), (erros >= 5 ? '\\' : ' '));
+    printf(" |              \n");
+    printf("_|___           \n\n");
 }
 
 void chutarletra(){  
@@ -51,15 +76,90 @@ void chutarletra(){
     printf("\nChute uma letra!");
     scanf(" %c", &chute);
     chute = toupper(chute);
-    chutes[tentativas] = chute;
-    tentativas++;   
-}
-
+    
+    int letra_repetida = 0;
+    for(int i = 0; i < tentativas; i++){
+        if(chutes[i]==chute){
+            letra_repetida = 1;
+            break;
+        }
+    }
+    
+    if(letra_repetida){
+        printf("\nLetra já chutada! Tente novamente\n\n");
+    }
+    
+    else{
+        int achou = 0;
+        for (int i = 0; i < strlen(palavrasecreta); i++){
+            if (palavrasecreta[i]==chute){
+            letras_acertada[palavrasecreta[i] - 'A'] = 1;
+            achou = 1;
+            }
+        }
+            
+        if(achou){
+            printf("\nVocê acertou a letra %c\n\n",chute);
+        }
+        else{
+            chutes[tentativas] = chute;
+            tentativas++;   
+            printf("\nLetra %c não encontrada na palavra.\n\n", chute);
+            }
+        }
+    }
 int main(void){
     escolherpalavra(); 
+    inicializar_letra_acertada();
+       
     do {    
+        desenhar_forca(); 
         verificaletra();
-        chutarletra();   
+        chutarletra();
+        
+        int letras_acertadas = 0;
+        for (int i = 0; i < strlen(palavrasecreta); i++) {
+            if (letras_acertada[palavrasecreta[i] - 'A'] == 1) {
+                letras_acertadas++;
+            }
+        }
+
+        if (letras_acertadas == strlen(palavrasecreta)) {
+            printf("       ___________      \n");
+		printf("      '._==_==_=_.'     \n");
+		printf("      .-\\:      /-.    \n");
+		printf("     | (|:.     |) |    \n");
+		printf("      '-|:.     |-'     \n");
+		printf("        \\::.    /      \n");
+		printf("         '::. .'        \n");
+		printf("           ) (          \n");
+		printf("         _.' '._        \n");
+		printf("        '-------'       \n\n");
+        printf("  Parabéns você ganhou! ");    
+            break;
+        }
+
+        if (tentativas >= MAX_TENTATIVAS) {
+            printf("    _______________         \n");
+		printf("   /               \\       \n"); 
+		printf("  /                 \\      \n");
+		printf("//                   \\/\\  \n");
+		printf("\\|   XXXX     XXXX   | /   \n");
+		printf(" |   XXXX     XXXX   |/     \n");
+		printf(" |   XXX       XXX   |      \n");
+		printf(" |                   |      \n");
+		printf(" \\__      XXX      __/     \n");
+		printf("   |\\     XXX     /|       \n");
+		printf("   | |           | |        \n");
+		printf("   | I I I I I I I |        \n");
+		printf("   |  I I I I I I  |        \n");
+		printf("   \\_             _/       \n");
+		printf("     \\_         _/         \n");
+		printf("       \\_______/           \n");
+        printf("\n     Você velhou!           ");    
+            break;
+        }
+        
     } while(1);
     return 0;
 }
